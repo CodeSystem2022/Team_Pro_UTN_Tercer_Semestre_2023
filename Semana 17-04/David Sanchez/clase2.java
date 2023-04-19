02385631b498e067786b4f6434c24cfa67b0db88
David <81187353+nob322@users.noreply.github.com>
2023-04-19 20:12:36 -0300

    Add files via upload
    Clase 2 del java

---
+++ Semana 17-04/David Sanchez/clase2.java
@@ -0,0 +1,18 @@
+package test;
+public class TestArgumentosVariables{
+    public static void main(String[ ] args){
+        imprimirNumeros(3,4,5);
+        imprimirNumeros(1,2);
+        variosParametros("Juan","Perez",7,8,9);
+
+    }
+    private static void variosParametros(String nombre, String apellido, int ...numeros){//siempre al final van los argumentos variables
+        System.out.println("Nombre: " + nombre + ", Apellido: " + apellido);
+        imprimirNumeros(numeros);
+    }
+    private static void imprimirNumeros(int ...numeros){
+        for(int i = 0;i < numeros.length; i++){
+            System.out.println("Elementos: " + numeros[i]);
+        }
+    }
+}
