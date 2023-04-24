9b45c52b5db7650abcaa206f9d3a51e9fe815607
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2023-04-24 19:28:23 -0300

    Update Javascript.js

--- Semana 03-04/Genre, Franco/Javascript.js
+++ Semana 03-04/Genre, Franco/Javascript.js
@@ -1,1 +1,16 @@
+// while
+let cont = 0;
+while(cont < 3){
+    console.log(cont); 
+    cont++;
+}
+console.log("Fin del ciclo while"); 
 
+//do while
+let cont = 0;
+do{
+    console.log(cont); 
+    cont++;
+} while(cont < 3);
+
+console.log("Fin del ciclo do while");
